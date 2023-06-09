092464365b8de13e8f24939023cb1a5ec92b0f75
Arman Jasuja <arman_jasuja@yahoo.com>
2023-06-09 16:05:55 +0400

    Set items function defined, and error handling done

--- vending_machine/vending_machine.c
+++ vending_machine/vending_machine.c
@@ -4,10 +4,11 @@
 typedef struct Item
 {
     char *item;
-    float num;
+    float price;
 }Item;
 
 void set_items(Item array[]);
+void print_sold_items(int index, float paid);
 
 int main()
 {
@@ -19,11 +20,50 @@ int main()
     set_items(arr);
     // Print current status
     int index;
-    float cost;
-    while(scanf("%d %.3f", &index, &cost) == 2)
+    float paid;
+
+    // Error handling
+    while (scanf("%d %.3f", &index, &paid) == 2)
     {
+        if (paid < 0 || index < 0)
+        {
+            fprintf(stderr, "Malformed expression\n");
+            return 1;
+        }
+
+        if (index > 2)
+        {
+            fprintf(stderr, "Invalid Item\n");
+            return 2;
+        }
+
+        if (paid < arr[index].price)
+        {
+            fprintf(stderr, "Not enough money\n");
+            return 3;
+        }
+
+        print_sold_items(index, paid);
 
     }
-
+    
+    // Missing inputs
+    printf("Malformed expression\n");
+    return 1;
     // EOF case
+}
+
+void set_items(Item array[])
+{
+    array[0].item = "cola";
+    array[0].price = 0.75;
+    array[1].item = "candybar";
+    array[1].price = 1.25;
+    array[2].item = "popcorn";
+    array[2].price = 0.50;
+}
+
+void print_sold_items(int index, int paid)
+{
+    
 }
