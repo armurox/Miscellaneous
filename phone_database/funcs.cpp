a5a3b9c4e31136deba4fd0420b4fbe56123a9530
Arman Jasuja <arman_jasuja@yahoo.com>
2023-09-01 18:03:25 +0400

    Helper files added

---
+++ phone_database/funcs.cpp
@@ -0,0 +1,10 @@
+#include "funcs.h"
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+void c_command(map<string, phone_nums> db)
+{
+    
+}
---
+++ phone_database/funcs.h
@@ -0,0 +1,15 @@
+#include <string>
+#include <map>
+#ifndef FUNCS_H
+#define FUNCS_H
+
+typedef map<string, string> phone_nums;
+void c_command(map<string, phone_nums> db);
+void d_command(map<string, phone_nums> db);
+void l_command(map<string, phone_nums> db);
+void p_command(map<string, phone_nums> db);
+void n_command(map<string, phone_nums> db);
+void x_command(map<string, phone_nums> db);
+void s_command(map<string, phone_nums> db);
+void r_command(map<string, phone_nums> db);
+#endif
