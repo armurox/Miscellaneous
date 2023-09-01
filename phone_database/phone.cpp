bc6c9aabac43ec6e6b10035a77e967aee19a6eee
Arman Jasuja <arman_jasuja@yahoo.com>
2023-09-01 18:03:06 +0400

    New Project Started

---
+++ phone_database/phone.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include "funcs.h"
+
+using namespace std;
+
+int main()
+{
+    map<string, phone_nums> phone_db;
+
+    cout << "Info: Welcome to the phone database!\n";
+    cout << "Info: Please enter a command\n";
+    char command;
+    cin >> command;
+    while (command != 'Q')
+    {
+        switch (command)
+        {
+            case 'C':
+                c_command(phone_db);
+                break;
+            
+            case 'D':
+                d_command(phone_db);
+                break;
+
+            case 'L':
+                l_command(phone_db);
+                break;
+
+            case 'P':
+                p_command(phone_db);
+                break;
+
+            case 'N':
+                n_command(phone_db);
+                break;
+
+            case 'X':
+                x_command(phone_db);
+                break;
+
+            case 'S':
+                s_command(phone_db);
+                break;
+
+            case 'R':
+                r_command(phone_db);
+                break;
+        }
+    }
+
+    cout << "Info: Thank you for using the phone database!" << endl;
+}
