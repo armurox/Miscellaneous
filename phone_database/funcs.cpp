29c870a819556abf392149cf0e00b73c8e523257
Arman Jasuja <arman_jasuja@yahoo.com>
2023-09-01 18:27:19 +0400

    C and L commands written

--- phone_database/funcs.cpp
+++ phone_database/funcs.cpp
@@ -1,10 +1,69 @@
 #include "funcs.h"
 #include <iostream>
 #include <map>
+#include <algorithm>
+#include <string>
 
 using namespace std;
 
-void c_command(map<string, phone_nums> db)
+void c_command(map<string, phone_nums> &db)
 {
-    
+    string lastname, firstname;
+    cin >> lastname >> firstname;
+    if (db.find(lastname + "," + firstname) != db.end())
+    {
+        cout << "Contact already exists\n";
+    }
+
+    else
+    {
+        db[lastname + "," + firstname] = {{"HOME", ""}, {"CELL", ""}, {"WORK", ""}, {"FAX", ""}, {"VOIP", ""}};
+    }
 }
+
+void l_command(map<string, phone_nums> &db)
+{
+    string lastname, firstname;
+    cin >> lastname >> firstname;
+    if (db.find(lastname + "," + firstname) != db.end())
+    {
+        cout << "Contact not found\n";
+    }
+
+    else
+    {
+        db.erase(lastname + "," + firstname);
+    }
+}
+
+void d_command(map<string, phone_nums> &db)
+{
+    return;
+}
+
+void p_command(map<string, phone_nums> &db)
+{
+    return;
+}
+
+void n_command(map<string, phone_nums> &db)
+{
+    return;
+}
+
+void x_command(map<string, phone_nums> &db)
+{
+    return;
+}
+
+void s_command(map<string, phone_nums> &db)
+{
+    return;
+}
+
+void r_command(map<string, phone_nums> &db)
+{
+    return;
+}
+
+
--- phone_database/funcs.h
+++ phone_database/funcs.h
@@ -3,13 +3,14 @@
 #ifndef FUNCS_H
 #define FUNCS_H
 
+using namespace std;
 typedef map<string, string> phone_nums;
-void c_command(map<string, phone_nums> db);
-void d_command(map<string, phone_nums> db);
-void l_command(map<string, phone_nums> db);
-void p_command(map<string, phone_nums> db);
-void n_command(map<string, phone_nums> db);
-void x_command(map<string, phone_nums> db);
-void s_command(map<string, phone_nums> db);
-void r_command(map<string, phone_nums> db);
+void c_command(map<string, phone_nums> &db);
+void d_command(map<string, phone_nums> &db);
+void l_command(map<string, phone_nums> &db);
+void p_command(map<string, phone_nums> &db);
+void n_command(map<string, phone_nums> &db);
+void x_command(map<string, phone_nums> &db);
+void s_command(map<string, phone_nums> &db);
+void r_command(map<string, phone_nums> &db);
 #endif
